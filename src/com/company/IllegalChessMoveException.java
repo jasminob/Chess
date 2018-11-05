6baea09c2ea1df006ad1fbd28c9acb897e39f98b
xembel <osmanbasic.jasmin@gmail.com>
2018-11-05 11:37:57 +0100

    new changes

--- src/com/company/IllegalChessMoveException.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-
-public class IllegalChessMoveException extends Exception {
-    IllegalChessMoveException(String s) {
-        super(s);
-    }
-}
-
---
+++ src/main/java/com/company/Chessturn.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+class Chessturn{
+    private ChessPiece.Color color;
+    private String fromPos;
+    private String toPos;
+
+    public Chessturn(ChessPiece.Color color, String fromPos, String toPos) {
+        this.color = color;
+        this.fromPos = fromPos;
+        this.toPos = toPos;
+    }
+
+    public ChessPiece.Color getColor() {
+        return color;
+    }
+
+    public String getFromPos() {
+        return fromPos;
+    }
+
+    public String getToPos() {
+        return toPos;
+    }
+
+}
